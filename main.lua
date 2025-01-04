ac03af77712cf46352827fb1aaf7f2321063e094
qwertyui <watchdogbestac@gmail.com>
2025-01-04 10:44:19 -0600

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,6 +1,110 @@
-getgenv().license = {
-    key = 'CAK_9KOUJp65N-ivoQUm-OppZgXgRsB8CbdR',
-    data = {}
-}
-getgenv().executetick = tick()
-return loadfile('vape/MainScript.lua')()
+repeat task.wait() until game:IsLoaded()
+if shared.vape then shared.vape:Uninject() end
+
+getgenv().void = function() end
+getgenv().inf = math.huge
+getgenv().request = request or http.request or function() end
+
+local vape
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and vape then
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	end
+	return res
+end
+local queue_on_teleport = queue_on_teleport or function() end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local cloneref = cloneref or function(obj)
+	return obj
+end
+local playersService = cloneref(game:GetService('Players'))
+
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newcatvape/profiles/commit.txt')..'/'..select(1, path:gsub('newcatvape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+
+local function finishLoading()
+	vape.Init = nil
+	vape:Load()
+	task.spawn(function()
+		repeat
+			vape:Save()
+			task.wait(10)
+		until not vape.Loaded
+	end)
+
+	local teleportedServers
+	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
+		if (not teleportedServers) and (not shared.VapeIndependent) then
+			teleportedServers = true
+			local teleportScript = [[
+				shared.vapereload = true
+				loadfile("newcatvape/main.lua")()
+			]]
+			if shared.VapeDeveloper then
+				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
+			end
+			if shared.VapeCustomProfile then
+				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+			end
+			vape:Save()
+			queue_on_teleport(teleportScript)
+		end
+	end))
+
+	if not shared.vapereload then
+		if not vape.Categories then return end
+		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
+			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+		end
+	end
+end
+
+if not isfile('newcatvape/profiles/gui.txt') then
+	writefile('newcatvape/profiles/gui.txt', 'new')
+end
+local gui = readfile('newcatvape/profiles/gui.txt')
+
+if not isfolder('newcatvape/assets/'..gui) then
+	makefolder('newcatvape/assets/'..gui)
+end
+vape = loadstring(downloadFile('newcatvape/guis/'..gui..'.lua'), 'gui')()
+shared.vape = vape
+
+if not shared.VapeIndependent then
+	loadstring(downloadFile('newcatvape/games/universal.lua'), 'universal')()
+	if isfile('newcatvape/games/'..game.PlaceId..'.lua') then
+		loadstring(readfile('newcatvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+	else
+		if not shared.VapeDeveloper then
+			local suc, res = pcall(function()
+				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/'..readfile('newcatvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+			end)
+			if suc and res ~= '404: Not Found' then
+				loadstring(downloadFile('newcatvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+			end
+		end
+	end
+	finishLoading()
+else
+	vape.Init = finishLoading
+	return vape
+end
